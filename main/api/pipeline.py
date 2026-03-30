a236e8c7b7b428c11f04bba036bac4cc612796ba
sbisset <47271923+sbisset@users.noreply.github.com>
2026-03-30 20:49:40 +0100

    Update pipeline.py

--- main/api/pipeline.py
+++ main/api/pipeline.py
@@ -6,18 +6,14 @@ def link_steam_account(backend, user, response, request, *args, **kwargs):
         return
 
     connect_user_id = request.session.get("steam_connect_user_id")
-    print("PIPELINE connect_user_id:", connect_user_id)
-
     if not connect_user_id:
         return
 
     steam_url = response.identity_url
     steam_id = steam_url.rstrip("/").split("/")[-1]
-    print("PIPELINE steam_id:", steam_id)
 
     User = get_user_model()
     connect_user = User.objects.get(id=connect_user_id)
-
     player, _ = Player.objects.get_or_create(user=connect_user)
 
     if Player.objects.filter(steam_id=str(steam_id)).exclude(user=connect_user).exists():
